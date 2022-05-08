7ea8c0b9c8b219c857ed0d96bc209d3192856567
yrkim <icho0405@gmail.com>
2022-05-08 10:23:42 +0900

    modify: textInput decorators

--- src/components/elem/textInput/TextInput.stories.tsx
+++ src/components/elem/textInput/TextInput.stories.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import TextInput from './TextInput';
 
+const withForm = (story: () => JSX.Element) => <form>{story()}</form>;
+
 export default {
 	title: '컴포넌트/TextInput',
 	component: TextInput,
-	decorators: [(story) => <form>{story()}</form>],
+	decorators: [withForm],
 } as ComponentMeta<typeof TextInput>;
 
 const Template: ComponentStory<typeof TextInput> = (args) => (
